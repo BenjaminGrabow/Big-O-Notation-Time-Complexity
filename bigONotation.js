f37721865a95bcc1fe7f127c7f9e80ad061ea467
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-07 12:22:27 +0200

    change in exponentialRuntime function

--- bigONotation.js
+++ bigONotation.js
@@ -39,7 +39,7 @@ const linearRuntime = array => {
 const exponentialRuntime = array => {
   for(let i = 0; i < array.length; i++) {
     for(let j = 0; j < array.length; j++) {
-      console.log(array[i] + array[j]);
+      console.log(array[i], array[j]);
     }
   }
 };
