f62ce359a940d0c04c82c3a8160806af2cbac828
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-21 12:47:19 +0200

    added comment to timeComplexity.js

---
+++ timeComplexity.js
@@ -0,0 +1,7 @@
+// -Improve Time Complexity only if the data set 
+// is big
+
+// -if you must improve the speed of your application
+
+// -if your data set is small, it is more important to 
+// improve the readability of your code 
