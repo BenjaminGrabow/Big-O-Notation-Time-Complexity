dedb6bc1716a7324cd41955e0f9c6b12791d880f
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-22 12:26:34 +0200

    added explanation about different time complexities

--- timeComplexity.js
+++ timeComplexity.js
@@ -5,3 +5,21 @@
 
 // -if your data set is small, it is more important to 
 // improve the readability of your code 
+
+
+
+Complexity of Common Operations 
+
+Complexity       Operation
+____________________________________________________
+O(1)            |Running statement
+
+O(1)            |Value look-up on an array, object, variable
+
+O(logn)         |Loop that cuts problem in half every iteration
+
+O(n)            |Looping through the values of an array
+
+O(n^2)          |Double nested loops
+
+O(n^3)          |triple nested loops
